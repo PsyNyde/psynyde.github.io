8650481dd8c072f4339b1aeda4115e411f72db16
smolbit <wahidislamlinad@gmail.com>
2022-07-01 20:10:22 +0600

    added theme(dark/light)🌚🌝

--- index.js
+++ index.js
@@ -4,3 +4,37 @@ console.log(`meow :3
 　l、ﾞ ~ヽ
 　じしf_, )ノ
 `)
+
+let theme= localStorage.getItem("darkTheme");
+
+const enableDarkMode = () => {
+  document.querySelector("body").classList.remove("light");
+  document.querySelector("body").classList.add("dark");
+  
+  icon = document.getElementById("btn-icon");
+  icon.removeAttribute("data-icon");
+  icon.setAttribute("data-icon","bx:sun");
+
+  localStorage.setItem("darkTheme", "enabled");
+  console.log('dark');
+};
+
+const disableDarkMode = () => {
+    document.querySelector("body").classList.remove("dark");
+    document.querySelector("body").classList.add("light");
+  icon = document.getElementById("btn-icon");
+  icon.removeAttribute("data-icon");
+  icon.setAttribute("data-icon","bx:moon");
+
+    localStorage.setItem("darkTheme", null);
+  console.log("white");
+};
+
+theme === "enabled" ? enableDarkMode() : disableDarkMode();
+
+const changetheme = () => {
+  theme = localStorage.getItem("darkTheme");
+
+      theme !== "enabled" ? enableDarkMode() : disableDarkMode();
+  console.log("done");
+}
